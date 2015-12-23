9e2c4b9f83084b365fa6b7e65a616e55ff71afac
Júlia Hentz <julia.hentz@gmail.com>
2015-12-23 11:35:16 +0100

    Modified server, added basic routing with static respons on / and /about

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ var express = require('express');
 
 var app = express();
 
+app.get('/', function(req, res){
+
+    res.send('This is the root route');
+
+});
+
+app.get('/about', function(req,res){
+
+   res.send('This is the about page');
+
+});
+
 
 app.listen(3000, function(){
 
